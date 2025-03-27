f349382b673d53454fd54d5359cf99f8dd81439a
TruongAn202 <nguyenvantruongan7745@gmail.com>
2025-03-27 17:24:22 +0700

    Update trang profile(progress, dropdown)

--- app/(main)/workspace/_components/CaNhanHoaList.tsx
+++ app/(main)/workspace/_components/CaNhanHoaList.tsx
@@ -12,64 +12,90 @@ import { CaNhanHoaContext } from '@/context/CaNhanHoaContext';
 import { BlurFade } from '@/components/magicui/blur-fade';
 import ThemMoiCaNhanHoa from './ThemMoiCaNhanHoa';
 
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
+import { LogOut, UserCircle2 } from 'lucide-react';
+import ProFile from './ProFile';
+
+
 function CaNhanHoaList() {
-    const{user} = useContext(XacThucContext);
-    const convex = useConvex();
+  const { user } = useContext(XacThucContext);
+  const convex = useConvex();
 
-    const [caNhanHoaList, setCaNhanHoaList]=useState<CANHANHOA[]>([]); // de set du lieu = result, nho la 1 danh sach
-    const {canhanhoa,setCaNhanHoa}=useContext(CaNhanHoaContext);
+  const [caNhanHoaList, setCaNhanHoaList] = useState<CANHANHOA[]>([]); // de set du lieu = result, nho la 1 danh sach
+  const { canhanhoa, setCaNhanHoa } = useContext(CaNhanHoaContext);
 
-    useEffect(() => {
-      user && GetUserCaNhanHoa();
-    }, [user&& canhanhoa==null]);//cap nhat ngay danh sach cac AI còn lại sau khi xoa
-    // Hàm này lấy danh sách người dùng cá nhân hóa từ Convex
-    const GetUserCaNhanHoa = async () => {
-      const result = await convex.query(api.userAiCaNhanHoa.GetAllUserCaNhanHoa, {
-          uid: user._id, // Truy vấn dữ liệu dựa trên ID người dùng
-      });
-      console.log(result);
-      setCaNhanHoaList(result);
-    };
+  const [openProfile, setOpenProfile]=useState(false);
+
+  useEffect(() => {
+    user && GetUserCaNhanHoa();
+  }, [user && canhanhoa == null]);//cap nhat ngay danh sach cac AI còn lại sau khi xoa
+  // Hàm này lấy danh sách người dùng cá nhân hóa từ Convex
+  const GetUserCaNhanHoa = async () => {
+    const result = await convex.query(api.userAiCaNhanHoa.GetAllUserCaNhanHoa, {
+      uid: user._id, // Truy vấn dữ liệu dựa trên ID người dùng
+    });
+    console.log(result);
+    setCaNhanHoaList(result);
+  };
   return (
     <div className='p-5 bg-secondary border-r-[1px] h-screen relative'>
-        <h2 className='font-bold text-lg'>Danh Sách AI Của Bạn</h2>
-        <ThemMoiCaNhanHoa>
-          <Button className='w-full mt-3 cursor-pointer'>+ Thêm AI Mới</Button>
-        </ThemMoiCaNhanHoa>
-        <Input className='bg-white mt-3' placeholder='Tìm kiếm'/>
-        <div className='mt-5'>
-            {/* dung canhanhoaList de hien thi nhung ai da chon chu khong  phai AiCaNhanHoaList la list AI All*/}
-            {caNhanHoaList.map((canhanhoa_,index)=>(
-              <BlurFade key={canhanhoa_.image} delay={0.25 + index * 0.05} inView>
-                {/* cho chữ va ảnh nằm trên 1 dòng gap-3 items-center */}
-                <div className={`p-2 mt-2 flex gap-3 items-center hover:bg-gray-200 hover:dark:bg-slate-700 rounded-xl cursor-pointer
-                  ${canhanhoa_.id==canhanhoa?.id&&'bg-gray-200'} 
-                `} 
-                //  ${canhanhoa_.id==canhanhoa?.id&&'bg-gray-200' nghia la khi 1 AI được chọn, background sẽ thành màu xám
-                key={index} onClick={()=>setCaNhanHoa(canhanhoa_)}>
-                    {/* image cua nextjs */}
-                    <Image src={canhanhoa_.image} alt={canhanhoa_.name}
-                        width={60}
-                        height={60}
-                        className='rounded-xl w-[60px] h-[60px] object-cover'
-                    />
-                    <div>
-                        <h2 className='font-bold'>{canhanhoa_.name}</h2>
-                        <h2 className='text-gray-600 dark:text-gray-300 text-sm'>{canhanhoa_.title}</h2>
-                    </div>
-                </div>
-                </BlurFade>
-            ))}
-        </div>
-        <div className='absolute bottom-10 flex gap-3 items-center hover:bg-gray-200 w-[90%] p-2 rounded-xl'>
+      <h2 className='font-bold text-lg'>Danh Sách AI Của Bạn</h2>
+      <ThemMoiCaNhanHoa>
+        <Button className='w-full mt-3 cursor-pointer'>+ Thêm AI Mới</Button>
+      </ThemMoiCaNhanHoa>
+      <Input className='bg-white mt-3' placeholder='Tìm kiếm' />
+      <div className='mt-5'>
+        {/* dung canhanhoaList de hien thi nhung ai da chon chu khong  phai AiCaNhanHoaList la list AI All*/}
+        {caNhanHoaList.map((canhanhoa_, index) => (
+          <BlurFade key={canhanhoa_.image} delay={0.25 + index * 0.05} inView>
+            {/* cho chữ va ảnh nằm trên 1 dòng gap-3 items-center */}
+            <div className={`p-2 mt-2 flex gap-3 items-center hover:bg-gray-200 hover:dark:bg-slate-700 rounded-xl cursor-pointer
+                  ${canhanhoa_.id == canhanhoa?.id && 'bg-gray-200'} 
+                `}
+              //  ${canhanhoa_.id==canhanhoa?.id&&'bg-gray-200' nghia la khi 1 AI được chọn, background sẽ thành màu xám
+              key={index} onClick={() => setCaNhanHoa(canhanhoa_)}>
+              {/* image cua nextjs */}
+              <Image src={canhanhoa_.image} alt={canhanhoa_.name}
+                width={60}
+                height={60}
+                className='rounded-xl w-[60px] h-[60px] object-cover'
+              />
+              <div>
+                <h2 className='font-bold'>{canhanhoa_.name}</h2>
+                <h2 className='text-gray-600 dark:text-gray-300 text-sm'>{canhanhoa_.title}</h2>
+              </div>
+            </div>
+          </BlurFade>
+        ))}
+      </div>
+
+      <DropdownMenu>
+        <DropdownMenuTrigger asChild>
+          <div className='absolute bottom-10 flex gap-3 items-center hover:bg-gray-200 w-[90%] p-2 rounded-xl cursor-pointer'>
           {/* tranh loi user?.picture co chuoi rong thi nextjs hien canh bao */}
-          {user?.picture ? (<Image src={user.picture} alt="user" width={35} height={35} className='rounded-full'/>) : null} 
+          {user?.picture ? (<Image src={user.picture} alt="user" width={35} height={35} className='rounded-full' />) : null}
           <div>
             <h2 className='font-bold'>{user?.name}</h2>
-            <h2 className='text-sm text-gray-400'>{user?.orderId?'VIP':'Miễn Phí'}</h2>
+            <h2 className='text-sm text-gray-400'>{user?.orderId ? 'VIP' : 'Miễn Phí'}</h2>
           </div>
-          
         </div>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent>
+          <DropdownMenuLabel>Tài khoản</DropdownMenuLabel>
+          <DropdownMenuSeparator />
+          <DropdownMenuItem onClick={()=>setOpenProfile(true)}><UserCircle2/> Thông tin</DropdownMenuItem>
+          <DropdownMenuItem><LogOut/> Đăng xuất</DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
+      {/* setOpenDialog={setOpenProfile} để nút X thoát hoạt động */}
+        <ProFile openDialog={openProfile} setOpenDialog={setOpenProfile}/>
     </div>
   )
 }
---
+++ app/(main)/workspace/_components/ProFile.tsx
@@ -0,0 +1,73 @@
+import React, { useContext } from 'react'
+
+import {
+    Dialog,
+    DialogClose,
+    DialogContent,
+    DialogDescription,
+    DialogHeader,
+    DialogTitle,
+    DialogTrigger,
+} from "@/components/ui/dialog"
+import { XacThucContext } from '@/context/XacThucContext'
+import Image from 'next/image';
+import { Progress } from '@/components/ui/progress';
+import { Button } from '@/components/ui/button';
+import { WalletIcon } from 'lucide-react';
+
+
+function ProFile({ openDialog, setOpenDialog }: any) {
+    const { user } = useContext(XacThucContext);
+    return (
+        // onOpenChange={setOpenDialog} để dùng cho nút X thoát hoạt động
+        <Dialog open={openDialog} onOpenChange={setOpenDialog}>
+            {/* <DialogTrigger>Open</DialogTrigger> */}
+            <DialogContent>
+                <DialogHeader>
+                    <DialogTitle>{ }</DialogTitle>
+                    <DialogDescription asChild>
+                        <div>
+                            <div className='flex gap-4 items-center'>
+                                <Image src={user.picture} alt='user' width={150} height={150}
+                                    className='w-[55px] h-[55px] rounded-full'
+                                />
+                                <div>
+                                    <h2 className='font-bold text-lg'>{user?.name}</h2>
+                                    <h2 className='text-gray-500'>{user?.email}</h2>
+                                </div>
+                            </div>
+                            <hr className='my-3'/>
+                            <div className='flex flex-col gap-2'>
+                                <h2 className='font-bold'>Điểm khả dụng</h2>
+                                <h2>0/0</h2>
+                                <Progress value={33} />
+                                <h2 className='flex justify-between font-bold text-lg'>Gói hiện tại
+                                    <span className='p-1 bg-gray-100 rounded-md font-normal'>{user?.orderId?'Miễn Phí': 'Cao Cấp'}</span>
+                                </h2>
+                            </div>
+                            <div className='p-4 border rounded-xl'>
+                                <div className='flex justify-between'>
+                                    <div>
+                                        <h2 className='font-bold text-lg'>Gói cao cấp</h2>
+                                        <h2>500,000 điểm</h2>
+                                    </div>
+                                    <h2 className='font-bold text-lg'>200k/Tháng</h2>
+                                </div>
+                                <hr className='my-3'/>
+                                <Button className='w-full'> <WalletIcon/> Nâng cấp(200k)</Button>
+                            </div>
+                            {/* <div className='mt-5 flex justify-end'>
+                                <DialogClose asChild>
+                                    <Button variant={'secondary'}>Thoát</Button>
+                                </DialogClose>
+                            </div> */}
+                        </div>
+                    </DialogDescription>
+                </DialogHeader>
+            </DialogContent>
+        </Dialog>
+
+    )
+}
+
+export default ProFile
