d979001b11c550dd503b9e566d9e66ba4a5a5116
TruongAn202 <nguyenvantruongan7745@gmail.com>
2025-05-03 10:48:39 +0700

    Update link ipn momo

--- app/api/payment/route.ts
+++ app/api/payment/route.ts
@@ -24,7 +24,9 @@ export async function POST(req: NextRequest) {
     const requestId = orderId;
     const orderInfo = 'Nâng cấp tài khoản gói cao cấp';
     const redirectUrl = 'http://localhost:3000/workspace';
-    const ipnUrl = 'https://27e5-171-232-66-106.ngrok-free.app/api/momo/ipn';
+    //const ipnUrl = 'https://27e5-171-232-66-106.ngrok-free.app/api/momo/ipn';
+    const ipnUrl = 'https://ai-canhanhoa-tr-an202.vercel.app/api/momo/ipn';
+    
     const extraData = userId;
 
     // Tạo rawSignature cho chữ ký
